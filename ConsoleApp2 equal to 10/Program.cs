bb40bfcbedf3666f53b127554377f0cae2a6b2de
Nisha R K <55803996+nishark90@users.noreply.github.com>
2025-03-05 20:51:48 +0530

    Add project files.

---
+++ ConsoleApp2 equal to 10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_equal_to_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 10) 
+            Console.WriteLine("The number is above 10");
+            else if (num < 10) 
+            Console.WriteLine("The number is below 10");
+            else 
+            Console.WriteLine("The number is equal to 10");
+            Console.ReadKey();
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        }
+    }
+}
